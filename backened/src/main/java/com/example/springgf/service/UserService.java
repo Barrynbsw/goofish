6b7fba4cf5f9ca2fdc1197f02621303a63ebc68e
barry <127286276+Barrynbsw@users.noreply.github.com>
2023-10-22 17:53:55 +0800

    category1

--- backened/src/main/java/com/example/springgf/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.springgf.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.springgf.entity.users;
-
-public interface UserService extends IService<users> {
-}
